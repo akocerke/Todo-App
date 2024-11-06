76327eb04e61002406f69b563d64a7c3ea3cda6c
akocerke <wickedbitcoin@protonmail.com>
2024-11-06 12:46:17 +0100

    update CSS

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,6 +1,10 @@
 "use client";
 import Link from "next/link";
-import { AiFillFacebook, AiFillTwitterCircle, AiFillInstagram } from "react-icons/ai";
+import {
+  AiFillFacebook,
+  AiFillTwitterCircle,
+  AiFillInstagram,
+} from "react-icons/ai";
 import { usePathname } from "next/navigation";
 
 export const Footer = () => {
@@ -17,14 +21,18 @@ export const Footer = () => {
         return "bg-gradient-to-r from-purple-500 to-pink-500"; // Registrierungsseite
       case "/logout":
         return "bg-gradient-to-r from-green-500 to-teal-800"; // Logout-Seite
-        case "/dashboard":
-          return "bg-gradient-to-r from-orange-700 to-lime-500"; // Dashboard-Seite
+      case "/dashboard":
+        return "bg-gradient-to-r from-orange-700 to-lime-500"; // Dashboard-Seite
+      case "/dashboard/tasks":
+        return "bg-gradient-to-r from-orange-700 to-lime-500"; // Dashboard-Tasks-Seite
+      case "/dashboard/settings":
+        return "bg-gradient-to-r from-orange-700 to-lime-500"; // Dashboard-Einstellungen-Seite
       case "/terms":
-          return "bg-gradient-to-r from-yellow-400 to-red-600" // Nutzerbedingungen-Seite
+        return "bg-gradient-to-r from-yellow-400 to-red-600"; // Nutzerbedingungen-Seite
       case "/privacypolicy":
-          return "bg-gradient-to-r from-purple-600 to-blue-400"  // Datenschutz-Seite
+        return "bg-gradient-to-r from-purple-600 to-blue-400"; // Datenschutz-Seite
       case "/imprint":
-          return "bg-gradient-to-r from-blue-400 to-purple-600"  // Impressum-Seite
+        return "bg-gradient-to-r from-blue-400 to-purple-600"; // Impressum-Seite
       default:
         return "bg-black bg-opacity-50"; // Standardfarbe für andere Seiten
     }
@@ -43,7 +51,10 @@ export const Footer = () => {
 
         <div className="mb-6 md:mb-0 md:w-1/3">
           <h3 className="text-lg font-bold">Rechtliches</h3>
-          <Link href="/privacypolicy" className="block text-sm hover:text-gray-300">
+          <Link
+            href="/privacypolicy"
+            className="block text-sm hover:text-gray-300"
+          >
             Datenschutz
           </Link>
           <Link href="/terms" className="block text-sm hover:text-gray-300">
@@ -57,19 +68,31 @@ export const Footer = () => {
         <div className="md:w-1/3 ">
           <h3 className="text-lg font-bold">Folge uns</h3>
           <div className="flex space-x-4 mt-2">
-            <Link href="https://facebook.com" target="_blank" className="hover:text-gray-300">
+            <Link
+              href="https://facebook.com"
+              target="_blank"
+              className="hover:text-gray-300"
+            >
               <AiFillFacebook className="h-6 w-6" />
             </Link>
-            <Link href="https://twitter.com" target="_blank" className="hover:text-gray-300">
+            <Link
+              href="https://twitter.com"
+              target="_blank"
+              className="hover:text-gray-300"
+            >
               <AiFillTwitterCircle className="h-6 w-6" />
             </Link>
-            <Link href="https://instagram.com" target="_blank" className="hover:text-gray-300">
+            <Link
+              href="https://instagram.com"
+              target="_blank"
+              className="hover:text-gray-300"
+            >
               <AiFillInstagram className="h-6 w-6" />
             </Link>
           </div>
         </div>
       </div>
-      
+
       <div className="mt-6 text-center text-sm relative z-10">
         <p>© 2024 ToDo APP. Alle Rechte vorbehalten.</p>
       </div>
